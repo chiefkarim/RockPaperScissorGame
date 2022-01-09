96d0dcb42ebd55b693ad102d5604a71fe5a0d75f
rudoyo <krimotop2016@gmail.com>
2022-01-09 02:34:32 -0800

    RockPaperCisorsGame.html and images/GameOver.png and images/Paper.png and images/Rock.png and images/scissor.png and javaScript.js and style.css

--- javaScript.js
+++ javaScript.js
@@ -1,35 +1,58 @@
+let computerSelection;
 function computerPlay(){
     let choices=["Rock","Paper","Scissor"];
-    return randomChoice=choices[Math.floor(Math.random()*choices.length)];
+   computerSelection=choices[Math.floor(Math.random()*choices.length)];
 }
-let computerSelection=computerPlay();
 
-
-function playerSelection(){
-    window.playerChoice=prompt("choose your weapon");
-   return "you selected "+ playerChoice.toLowerCase();
-   
+let playerChoice;
+function playerSelection(clicked_id){
+    playerChoice=clicked_id;
 }
-function game(){
-    for(i=0;i<5;i++){
-        playerSelection();
-        console.log(playRound());
+
+function play(clicked_id){
+    if(!(cScore===5 || pScrore===5)){
+    playerSelection(clicked_id)
+    computerPlay()
+    scoore();
+
+    if(cScore===5 || (pScrore===5)){
+        if(cScore===5){scoor.textContent+=' you Loose'}
+        else if(pScrore===5){scoor.textContent+=` you Win`}
+else return
     }
 }
+}
+
+let scoor=document.querySelector(".scoor");
 let cScore=0;
 let pScrore=0;
 
-function playRound(){
-    if (computerSelection ==="Rock" && playerChoice==="rock"){return "it\'s a tie, you both chose Rock tie. computer:"+cScore+" player:"+pScrore}
-    else if(computerSelection==="Rock" && playerChoice==="paper"){return "you win, computer chose Rock. computer:"+cScore+" player:"+(pScrore += 1)}
-    else if(computerSelection==="Rock" && playerChoice==="scissor"){return "you loose, computer chose Rock. computer:"+(cScore += 1)+" player:"+pScrore}
-    else if(computerSelection==="Paper" && playerChoice==="paper"){return "it\'s a tie, you both chose Paper. computer:"+cScore+" player:"+pScrore}
-    else if(computerSelection==="Paper" && playerChoice==="rock"){return "you loose, computer chose paper. computer:"+(cScore += 1)+" player:"+pScrore}
-    else if(computerSelection==="Paper" && playerChoice==="scissor"){return "you win, computer chose paper. computer:"+cScore+" player:"+(pScrore += 1)}
-    else if(computerSelection==="Scissor" && playerChoice==="scissor"){return "it\'s a tie, you both chose Scissor. computer:"+cScore+" player:"+pScrore}
-    else if(computerSelection==="Scissor" && playerChoice==="rock"){return "you win, computer chose Scissor. computer:"+cScore+" player:"+(pScrore += 1)}
-    else if(computerSelection==="Scissor" && playerChoice==="paper"){return "you loose, computer chose Scissor. computer:"+(cScore += 1)+" player:"+pScrore}
+function scoore(){
+    if (computerSelection ==="Rock" && playerChoice==="rock"){scoor.textContent=`computer:${cScore}-${pScrore}:player*it's a tie`}
+    else if(computerSelection==="Rock" && playerChoice==="paper"){scoor.textContent=`computer:${cScore}-${pScrore+=1}:player`}
+    else if(computerSelection==="Rock" && playerChoice==="scissor"){scoor.textContent=`computer:${cScore+=1}-${pScrore}:player`}
+    else if(computerSelection==="Paper" && playerChoice==="paper"){scoor.textContent=`computer:${cScore}-${pScrore}:player*it's a tie`}
+    else if(computerSelection==="Paper" && playerChoice==="rock"){scoor.textContent=`computer:${cScore+=1}-${pScrore}:player`}
+    else if(computerSelection==="Paper" && playerChoice==="scissor"){scoor.textContent=`computer:${cScore}-${pScrore+=1}:player`}
+    else if(computerSelection==="Scissor" && playerChoice==="scissor"){scoor.textContent=`computer:${cScore}-${pScrore}:player*it's a tie`}
+    else if(computerSelection==="Scissor" && playerChoice==="rock"){scoor.textContent=`computer:${cScore}-${pScrore+=1}:player`}
+    else if(computerSelection==="Scissor" && playerChoice==="paper"){scoor.textContent=`computer:${cScore+=1}-${pScrore}:player`}
     else {return "the weapon you chose is unavailable, please choose one of the \"Rock,Paper,Scissor\""}
 }
-game();
 
+function showWeapons(){
+    let weapons=Array.from(document.querySelectorAll(".weapons"));
+let btn=document.querySelector('button');
+let scoor=document.querySelector(".scoor");
+let i;
+
+for(i=0;i<weapons.length;i++){
+
+      weapons[i].style.display = "inline";
+    
+}
+
+btn.style.display='none';
+    scoor.style.display='flex';
+}
+  
